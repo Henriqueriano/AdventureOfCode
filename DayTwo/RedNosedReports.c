ef87d3c9f070a8af23b8713e07a2b3fa56b6b01c
Daniel Henrique <daniel.neves0021@gmail.com>
2024-12-02 11:54:36 -0300

    feat: initiated the second day.

--- DayTwo/RedNosedReports.c
+++ DayTwo/RedNosedReports.c
@@ -1,14 +1,109 @@
 #include <stdio.h>
-int calcDiffInnerElements(int** list, int l);
+void calcDiffInnerElements(int list[][3], int l);
 int main ( int argc, char argv[]) 
 {
-				int a[][5] = {{1,2,3,4,5},{4,5,6}};
-				calcDiffInnerElements(a[5],5);
+				int a[][3] = {{1,2,3},{4,5,4}};
+				calcDiffInnerElements(a,2);
 				return 1;
 }
-int calcDiffInnerElements(int** list, int l)
+void calcDiffInnerElements(int list[][3], int l)
 {
-				for ( int i = 0 ; i < l; i ++ ) 
-								for (int j = 0 ; j < l ; j ++)
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				// Burro;
+				for ( int i = 0 ; i < l; i ++ )
+								for (int j = 0 ; j < 3 ; j ++)
 												printf("%i ", list[i][j]);
 }
